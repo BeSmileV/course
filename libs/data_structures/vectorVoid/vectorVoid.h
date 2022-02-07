3f7af08b1685c2d1c4f78a9551e365705585f198
Sergey <besmilev@gmail.com>
2022-02-07 22:40:11 +0300

    memory usage of vectorVoid

--- libs/data_structures/vectorVoid/vectorVoid.h
+++ libs/data_structures/vectorVoid/vectorVoid.h
@@ -2,6 +2,10 @@
 #define COURSE__VECTORVOID_H
 
 #include <limits.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 typedef struct vectorVoid {
     void *data;             // указатель на нулевой элемент вектора
@@ -9,5 +13,22 @@ typedef struct vectorVoid {
     size_t capacity;        // вместимость вектора
     size_t baseTypeSize;    // размер базового типа
 } vectorVoid;
+
+/// возвращает пустую структуру vector максимальным размером n
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+/// изменяет количество выделяемой памяти под вектор v на newCapacity,
+/// если newCapacity равно 0, то в data будет NULL, если newCapacity
+/// меньше size, size будет равно newCapacity
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+/// освобождает память вектора v, выделенную под неиспользуемые элементы
+void shrinkToFitV(vectorVoid *v);
+
+/// удаляет элементы вектора v
+void clearV(vectorVoid *v);
+
+/// освобождает память, выделенную вектору v
+void deleteVectorV(vectorVoid *v);
 
 #endif
