4686a0b08c006dcd599fcd981974a5ef79a67146
Sergey <besmilev@gmail.com>
2022-02-13 22:42:46 +0300

    functions for ordering

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -47,10 +47,45 @@ void swapRows(matrix m, int i1, int i2) {
     m.values[i2] = t;
 }
 
-void swapColumns(matrix m, int j1, int j2){
-    for(size_t i = 0; i < m.nCols; i++){
+void swapColumns(matrix m, int j1, int j2) {
+    for (size_t i = 0; i < m.nCols; i++) {
         int t = m.values[i][j1];
         m.values[i][j1] = m.values[i][j2];
         m.values[i][j2] = t;
     }
+}
+
+void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int *, int)) {
+    int *a = (int *) malloc(sizeof(int) * m.nRows);
+    for(size_t i = 0; i < m.nRows; i++)
+        a[i] = criteria(m.values[i], m.nCols);
+    for(int i = 1; i < m.nRows; i++){
+        int j = i;
+        while(j > 0 && a[j - 1] > a[i]){
+            swapRows(m, j, j - 1);
+            j--;
+        }
+        swapRows(m, i, j);
+    }
+    free(a);
+}
+
+void insertionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int *, int)){
+    int *buffer = (int *) malloc(sizeof(int) * m.nRows);
+    int *a = (int *) malloc(sizeof(int) * m.nCols);
+    for(size_t i = 0; i < m.nCols; i++) {
+        for(size_t j = 0; j < m.nRows; j++)
+            buffer[j] = m.values[j][i];
+        a[i] = criteria(buffer, m.nRows);
+    }
+    free(buffer);
+    for(int i = 1; i < m.nCols; i++){
+        int j = i;
+        while(j > 0 && a[j - 1] > a[i]){
+            swapRows(m, j, j - 1);
+            j--;
+        }
+        swapColumns(m, i, j);
+    }
+    free(a);
 }
